a7ef3d00e4c5e38e5c9a858ee39c381bae18946d
rushia <jonasg.13452@gmail.com>
2021-12-08 15:58:22 -0500

    Update day6.py

--- day6.py
+++ day6.py
@@ -2,22 +2,25 @@ def findNumber(lanterns):
   total = 0
   cur_arr = []
   for lantern in lanterns:
+    cur_arr = []
     cur_arr.append(lantern)
-    for day in range(80):
+    for day in range(200):
       for i in range(len(cur_arr)):
         if cur_arr[i] > 0:
           cur_arr[i] -= 1
-         else:
+        else:
           cur_arr[i] = 6
           cur_arr.append(8)
     total += len(cur_arr)
   return total
+
+  # make new fish with number and spawn day and calculate from there
         
 def main():
   input = '''\
-  
+3,4,3,1,2
 '''
-  lanterns = input.split(",")
+  lanterns = [int(num) for num in input.split(",")]
   print(findNumber(lanterns))
   
 if __name__ == "__main__":
