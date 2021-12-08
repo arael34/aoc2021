bafd3014bcfd134630f63849231702bb9950cb91
rushia <94145174+rushia9@users.noreply.github.com>
2021-12-08 12:51:26 -0500

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,16 @@
+def findNumber(lanterns):
+  total = 0
+  cur_arr = 0
+  for lantern in lanterns:
+    for day in range(80):
+      if lantern
+
+def main():
+  input = '''\
+  
+'''
+  lanterns = input.split(",")
+  print(findNumber(lanterns))
+  
+if __name__ == "__main__":
+  main()
