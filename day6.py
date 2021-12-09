efe84c9d373031e7f4b0c05e26eadd766d88c8a8
rushia <jonasg.13452@gmail.com>
2021-12-09 15:31:07 -0500

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,73 +1,19 @@
-'''
-def findNumber(lanterns):
-  cur_arr = []
-  total = 0
-  for lantern in lanterns:
-    cur_arr = []
-    cur_arr.append(lantern)
-    for day in range(100):
-      for i in range(len(cur_arr)):
-        if cur_arr[i] > 0:
-          cur_arr[i] -= 1
-        else:
-          cur_arr[i] = 6
-          cur_arr.append(8)
-    total += len(cur_arr)
-  return total
-'''
-
-# another solution might be to find the amount of 1s, 2s, etc and go from there
-'''
 def findNumber(lanterns):
   arr = []
   new = 0
-  for lantern in lanterns:    # or for i in range(9): arr[i] = lanterns.count(i or whatever)
-    for i in range(9):
-      if lantern == i:
-        arr[i] += 1
-  for day in range(80):
+  for i in range(9):
+    arr.append(lanterns.count(i))
+  for day in range(256):
     new = arr.pop(0)
-    arr[6] += new; arr[8] += new
+    arr[6] += new; arr.append(new)
   return sum(arr)
-'''
-  # recursion solution
-def findNumber(lanterns, daysLeft):
-  cur_arr = []
-  if (daysLeft <= 100):
-    total = 0
-    for lantern in lanterns:
-      cur_arr = []
-      cur_arr.append(lantern)
-      for day in range(100):
-        for i in range(len(cur_arr)):
-          if cur_arr[i] > 0:
-            cur_arr[i] -= 1
-          else:
-            cur_arr[i] = 6
-            cur_arr.append(8)
-      total += len(cur_arr)
-    return total
-  else:
-    arr = []
-    for lantern in lanterns:
-      cur_arr = []
-      cur_arr.append(lantern)
-      for day in range(100):
-        for i in range(len(cur_arr)):
-          if cur_arr[i] > 0:
-            cur_arr[i] -= 1
-          else:
-            cur_arr[i] = 6
-            cur_arr.append(8)
-      arr += cur_arr
-    return findNumber(arr, daysLeft - 100) #idk if this is right
-  
+
 def main():
   input = '''\
-3,4,3,1,2
+1,1,1,1,3,1,4,1,4,1,1,2,5,2,5,1,1,1,4,3,1,4,1,1,1,1,1,1,1,2,1,2,4,1,1,1,1,1,1,1,3,1,1,5,1,1,2,1,5,1,1,1,1,1,1,1,1,4,3,1,1,1,2,1,1,5,2,1,1,1,1,4,5,1,1,2,4,1,1,1,5,1,1,1,1,5,1,3,1,1,4,2,1,5,1,2,1,1,1,1,1,3,3,1,5,1,1,1,1,3,1,1,1,4,1,1,1,4,1,4,3,1,1,1,4,1,2,1,1,1,2,1,1,1,1,5,1,1,3,5,1,1,5,2,1,1,1,1,1,4,4,1,1,2,1,1,1,4,1,1,1,1,5,3,1,1,1,5,1,1,1,4,1,4,1,1,1,5,1,1,3,2,2,1,1,1,4,1,3,1,1,1,2,1,3,1,1,1,1,4,1,1,1,1,2,1,4,1,1,1,1,1,4,1,1,2,4,2,1,2,3,1,3,1,1,2,1,1,1,3,1,1,3,1,1,4,1,3,1,1,2,1,1,1,4,1,1,3,1,1,5,1,1,3,1,1,1,1,5,1,1,1,1,1,2,3,4,1,1,1,1,1,2,1,1,1,1,1,1,1,3,2,2,1,3,5,1,1,4,4,1,3,4,1,2,4,1,1,3,1
 '''
   lanterns = [int(num) for num in input.split(",")]
-  print(findNumber(lanterns, 80))
+  print(findNumber(lanterns))
   
 if __name__ == "__main__":
   main()
