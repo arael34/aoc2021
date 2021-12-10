d04d590a802617ebb259e4d4534974d241f6992c
rushia <94145174+rushia9@users.noreply.github.com>
2021-12-10 12:01:48 -0500

    Create day9.py

---
+++ day9.py
@@ -0,0 +1,19 @@
+ input = '''\
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+'''
+
+def adjacent():
+  return 1
+
+def find_risk():
+  return 1
+
+def main()
+  print(find_risk())
+
+if __name__ == "__main__":
+  main()
